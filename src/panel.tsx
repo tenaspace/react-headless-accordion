c5b0c327b71c48705392e0bcb688a49cbc80e626
tenaspace <tenaspace@gmail.com>
2023-03-15 22:51:47 +0700

    fix bug add style panel

--- src/panel.tsx
+++ src/panel.tsx
@@ -16,13 +16,16 @@ const HeadlessAccordionPanel = ({ as = `div`, ...props }: IHeadlessAccordionPane
     if (ref && ref.current) {
       if (!firstRender) {
         if (open) {
-          ref.current.style.cssText = `display: block; overflow: hidden; max-height: 0;`
+          ref.current.style.display = `block`
+          ref.current.style.overflow = `hidden`
+          ref.current.style.maxHeight = `0`
           ref.current.style.maxHeight = `${ref.current.scrollHeight}px`
         } else {
           ref.current.style.maxHeight = `${ref.current.scrollHeight}px`
           const timeOut = setTimeout(() => {
             if (ref && ref.current) {
-              ref.current.style.cssText = `overflow: hidden; max-height: 0;`
+              ref.current.style.overflow = `hidden`
+              ref.current.style.maxHeight = `0`
             }
           }, 1)
           return () => {
@@ -37,10 +40,12 @@ const HeadlessAccordionPanel = ({ as = `div`, ...props }: IHeadlessAccordionPane
     if (ref && ref.current) {
       if (!firstRender) {
         if (open) {
-          ref.current.style.cssText = ``
+          ref.current.style.display = ``
         } else {
-          ref.current.style.cssText = `display: none`
+          ref.current.style.display = `none`
         }
+        ref.current.style.overflow = ``
+        ref.current.style.maxHeight = ``
       }
     }
   })
